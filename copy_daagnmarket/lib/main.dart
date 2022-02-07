d03670aaabd89e44f3bcc55c076b07d68c49bf6b
minwoo jung <jungmonster@navmer.com>
2022-02-08 02:17:23 +0900

    Feature : update step 1, 2

---
+++ copy_daagnmarket/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:copy_daagnmarket/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        // primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ copy_daagnmarket/lib/pages/home.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Map<String, String>> datas = [
+    {
+      "cid": "1",
+      "image": "assets/images/ara-1.jpg",
+      "title": "네메시스 축구화275",
+      "location": "제주 제주시 아라동",
+      "price": "30000",
+      "likes": "2"
+    },
+    {
+      "cid": "2",
+      "image": "assets/images/ara-2.jpg",
+      "title": "LA갈비 5kg팔아요~",
+      "location": "제주 제주시 아라동",
+      "price": "100000",
+      "likes": "5"
+    },
+    {
+      "cid": "3",
+      "image": "assets/images/ara-3.jpg",
+      "title": "치약팝니다",
+      "location": "제주 제주시 아라동",
+      "price": "5000",
+      "likes": "0"
+    },
+    {
+      "cid": "4",
+      "image": "assets/images/ara-4.jpg",
+      "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
+      "location": "제주 제주시 아라동",
+      "price": "2500000",
+      "likes": "6"
+    },
+    {
+      "cid": "5",
+      "image": "assets/images/ara-5.jpg",
+      "title": "디월트존기임팩",
+      "location": "제주 제주시 아라동",
+      "price": "150000",
+      "likes": "2"
+    },
+    {
+      "cid": "6",
+      "image": "assets/images/ara-6.jpg",
+      "title": "갤럭시s10",
+      "location": "제주 제주시 아라동",
+      "price": "180000",
+      "likes": "2"
+    },
+    {
+      "cid": "7",
+      "image": "assets/images/ara-7.jpg",
+      "title": "선반",
+      "location": "제주 제주시 아라동",
+      "price": "15000",
+      "likes": "2"
+    },
+    {
+      "cid": "8",
+      "image": "assets/images/ara-8.jpg",
+      "title": "냉장 쇼케이스",
+      "location": "제주 제주시 아라동",
+      "price": "80000",
+      "likes": "3"
+    },
+    {
+      "cid": "9",
+      "image": "assets/images/ara-9.jpg",
+      "title": "대우 미니냉장고",
+      "location": "제주 제주시 아라동",
+      "price": "30000",
+      "likes": "3"
+    },
+    {
+      "cid": "10",
+      "image": "assets/images/ara-10.jpg",
+      "title": "멜킨스 풀업 턱걸이 판매합니다.",
+      "location": "제주 제주시 아라동",
+      "price": "50000",
+      "likes": "7"
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbarWidget(),
+      body: _bodyWidget(),
+      // bottomNavigationBar: Container(),
+    );
+  }
+
+  final oCcy = new NumberFormat("#,###", "ko_KR");
+  String calculString(String priceString) {
+    return "${oCcy.format(int.parse(priceString))}원";
+  }
+
+  Widget _bodyWidget() {
+    return ListView.separated(
+      itemCount: datas.length,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+                child: Image.asset(
+                  datas[index]["image"] ?? '',
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        datas[index]["title"] ?? '',
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        datas[index]["location"] ?? '',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ),
+                      Text(
+                        calculString(datas[index]["price"] ?? ''),
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/svg/heart_off.svg",
+                            width: 13,
+                            height: 13,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            datas[index]["likes"] ?? '',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 1,
+          color: Colors.black.withOpacity(0.5),
+        );
+      },
+    );
+  }
+
+  AppBar _appbarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () {
+          print("click");
+        },
+        child: Row(
+          children: const [
+            Text('아라동'),
+            Icon(
+              Icons.arrow_drop_down,
+            ),
+          ],
+        ),
+      ),
+      elevation: 1,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.tune),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/svg/bell.svg",
+            width: 22,
+          ),
+        ),
+      ],
+    );
+  }
+}
